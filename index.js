9c30c086e27dfffcda0597c5aca76451eb2727da
theniceguywastaken <99677066+theniceguywastaken@users.noreply.github.com>
2022-11-15 11:39:05 +0100

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+let counter = 0;
+
+function show() {
+  counter++;
+  const el = <h1>The counter is now: {counter}</h1>;
+  ReactDOM.render(el, document.getElementById('root'));
+}
+
+do {
+  setInterval(show, 1000);
+} while (counter < 11);
